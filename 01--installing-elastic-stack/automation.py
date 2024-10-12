ee7e27ce2f38980c11bb17259c5b63ed16bd887f
ax-va <vasiliev.informatik@t-online.de>
2024-10-12 21:26:04 +0200

    update

--- 01--installing-elastic-stack/automation.py
+++ 01--installing-elastic-stack/automation.py
@@ -6,19 +6,19 @@ from pathlib import Path
 with open('../secret.yml', 'r') as file:
     secret = yaml.safe_load(file)
 
-ELASTICSEARCH_DIRS = {
+ELASTICSEARCH_DIR_PATHS = {
     "main": Path("/bin/elasticsearch"),
     "cold": Path("/bin/elasticsearch-node-cold"),
     "frozen": Path("/bin/elasticsearch-node-frozen"),
     "master": Path("/bin/elasticsearch-node-master"),
     "ml": Path("/bin/elasticsearch-node-ml"),
 }
-KIBANA_DIR = Path("/bin/kibana")
+KIBANA_DIR_PATH = Path("/bin/kibana")
 ELASTICSEARCH_PACKAGE = "elasticsearch-8.15.1"
 KIBANA_PACKAGE = "kibana-8.15.1"
 PACKAGE_SPECIFICATION = "-linux-x86_64.tar.gz"
-PATHS_TO_INSTALLED_PACKAGES = [dir_ / ELASTICSEARCH_PACKAGE for dir_ in ELASTICSEARCH_DIRS.values()]
-PATHS_TO_INSTALLED_PACKAGES.extend([KIBANA_DIR / KIBANA_PACKAGE])
+PATHS_TO_INSTALLED_PACKAGES = [dir_path / ELASTICSEARCH_PACKAGE for dir_path in ELASTICSEARCH_DIR_PATHS.values()]
+PATHS_TO_INSTALLED_PACKAGES.extend([KIBANA_DIR_PATH / KIBANA_PACKAGE])
 
 
 def execute_command_with_sudo(command: str):
@@ -51,14 +51,14 @@ def delete_all_elastic_stack_elements():
 
 
 def extract_all_elastic_stack_elements_from_tar_gz():
-    for dir_ in ELASTICSEARCH_DIRS.values():
+    for dir_path in ELASTICSEARCH_DIR_PATHS.values():
         filename = ELASTICSEARCH_PACKAGE + PACKAGE_SPECIFICATION
-        print("Extracting:", str(dir_ / filename))
-        extract_from_tar_gz(dir_, filename)
-    dir_ = KIBANA_DIR
+        print("Extracting:", str(dir_path / filename))
+        extract_from_tar_gz(dir_path, filename)
+    dir_path = KIBANA_DIR_PATH
     filename = KIBANA_PACKAGE + PACKAGE_SPECIFICATION
-    print("Extracting:", str(dir_ / filename))
-    extract_from_tar_gz(dir_, filename)
+    print("Extracting:", str(dir_path / filename))
+    extract_from_tar_gz(dir_path, filename)
 
 
 if __name__ == "__main__":
